44e89c9a863ad51c858adf0db44695cd2f5b5dcc
igor.kiria <yisgit@gmail.com>
2022-10-14 06:12:03 +0600

    Настройка баннеров для среды разработки

--- local/templates/b2b_template/components/dj.b2b/b2b.slider/.default/result_modifier.php
+++ local/templates/b2b_template/components/dj.b2b/b2b.slider/.default/result_modifier.php
@@ -1,8 +1,20 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true){die();}
+$res = CIBlock::GetList(
+    Array(),
+    Array(
+        'ACTIVE'=>'Y',
+        "CODE"=>'b2b_slider'), true
+);
+
+while($ar_res = $res->Fetch())
+{
+    $CODE_ID=$ar_res['ID'];
+}
+print_r($CODE_ID);
 
 $rsBanners = \Bitrix\Iblock\ElementTable::getList([
         'filter' => array(
-            'IBLOCK_ID' => 14,
+            'IBLOCK_ID' => $CODE_ID,
             'ACTIVE' => 'Y'
         ),
         'select' => array(
