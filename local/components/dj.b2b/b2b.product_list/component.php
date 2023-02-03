76463bb66e5339813367b9d4804ffc0d41c68780
Pyoss <yisgit@gmail.com>
2023-02-03 12:41:04 +0400

    Update component.php

--- local/components/dj.b2b/b2b.product_list/component.php
+++ local/components/dj.b2b/b2b.product_list/component.php
@@ -1,13 +1,5 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-/** @var CBitrixComponent $this */
-/** @var array $arParams */
-/** @var array $arResult */
-/** @var string $componentPath */
-/** @var string $componentName */
-/** @var string $componentTemplate */
-/** @global CDatabase $DB */
-/** @global CUser $USER */
-/** @global CMain $APPLICATION */
+
 include_once $_SERVER['DOCUMENT_ROOT'] . $componentPath . '/B2BCatalog.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . $componentPath . '/B2BDetails.php';
 use \B2BAjax\B2BCatalog;
